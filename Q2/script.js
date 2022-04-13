ee522109df77282ab1c98d6fff6d8113352703dd
Parvin khan <58837691+parveen11498@users.noreply.github.com>
2022-04-13 14:10:18 +0530

    Add files via upload

---
+++ Q2/script.js
@@ -0,0 +1,47 @@
+let MyResume={
+    "Profile": {
+      "name": "Parvin khan",
+      "email": "parveen4900613@gmail.com",
+      "phone": 9923058601,
+      "degree": "B.VOC(software Dev)",
+      "location": {
+        "address": "colva-GOa",
+        "postalCode": 403708,
+        "city": "Margao",
+        "state": "Goa",
+        "country": "India"
+      },
+      
+    },
+    
+    "Qualifications": [
+      {
+        "institution": "Parvatibai of chowgules Arts and science",
+        "department": "computer science",
+        "studyType": "fulltime",
+        "batch start year": 2019,
+        "batch end year": 2021,
+        
+      }
+    ],
+    "skills": [
+      {
+        "coding programs": "python,javascript,html,JSON,etc",
+        "level": "beginner",
+        "project": [
+          "one stop service website"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "English,Hindi,konkani",
+      }
+    ],
+    "interests": [
+      {
+        "name": "music,reading",
+      }
+    ]
+  }
+  console.log(MyResume);
